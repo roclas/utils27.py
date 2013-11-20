09dccc598a431150f21c7e59fd9cee200e5897f5
carlos <carlos@beeva.(none)>
2013-11-20 15:42:48 +0100

    minor changes

--- language_analysis/test1_sentiment.py
+++ language_analysis/test1_sentiment.py
@@ -14,28 +14,33 @@ neg_tweets = [('I do not like this car', 'negative'),
               ('I feel tired this morning', 'negative'),
               ('I am not looking forward to the concert', 'negative'),
               ('He is my enemy', 'negative')]
+
+test_tweets=['I love this chair',
+	     'I am tired',
+             'You are amazing']
+
 tweets = []
 for (words, sentiment) in pos_tweets + neg_tweets:
     words_filtered = [e.lower() for e in words.split() if len(e) >= 3] 
     tweets.append((words_filtered, sentiment))
-
 #print tweets
 
-def get_words_in_tweets(tweets):
+def get_all_words_in_tweets(tweets):
     all_words = []
     for (words, sentiment) in tweets:
       all_words.extend(words)
     return all_words
+#print get_all_words_in_tweets(tweets) # repiting words
 
 def get_word_features(wordlist):
     wordlist = nltk.FreqDist(wordlist)
     word_features = wordlist.keys()
     return word_features
+allwords=get_all_words_in_tweets(tweets)
+word_features=get_word_features(allwords)
+#print word_features #all words (no repetition)
 
-
-word_features=get_word_features(get_words_in_tweets(tweets))
-
-def extract_features(document):
+def extract_features(document): #extracts the TRAINED words from doc
     document_words = set(document)
     features = {}
     for word in word_features:
@@ -43,7 +48,7 @@ def extract_features(document):
     return features
 
 #for t in tweets:
-	#print extract_features(get_words_in_tweets([t]))
+	#print extract_features(allwords)
 
 
 training_set = nltk.classify.apply_features(extract_features, tweets)
@@ -55,5 +60,5 @@ classifier = nltk.NaiveBayesClassifier.train(training_set)
 tweet='Larrry is my friend'
 print "%s -> %s" %( tweet,classifier.classify(extract_features(tweet.split())))
 
-for (t,s) in neg_tweets+pos_tweets:
+for t in test_tweets:
 	print "%s -> %s" %( t,classifier.classify(extract_features(t.split())))
